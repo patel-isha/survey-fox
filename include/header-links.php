ac2722f3bbeb815675abf586abbe5b2bd40b5703
Isha Patel <patelisha307@gmail.com>
2024-05-02 16:28:28 +0100

    Added user form for travel survey

--- include/header-links.php
+++ include/header-links.php
@@ -4,6 +4,7 @@
     <title>Survey Fox</title>
     <link rel="shortcut icon" href="assets/img/logo/favicon.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
     <link rel="stylesheet" href="assets/css/form.css">
     <link rel="stylesheet" href="assets/css/common-class.css">
