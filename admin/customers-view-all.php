c280e70529f3826b06bdbda1c6e6f01c4c716253
meemansa <goswamimeemansa@gmail.com>
2024-05-21 19:45:08 +0100

    dashboard admin changes

--- admin/customers-view-all.php
+++ admin/customers-view-all.php
@@ -1,10 +1,13 @@
 <!doctype html>
 <html lang="en">
-<?php 
+<?php
 include 'includes/header.php';
-include "../config/connection.php"; 
+include "../config/connection.php";
 ?>
 
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.0/xlsx.full.min.js"></script>
+</head>
 
 
 <body>
@@ -40,22 +43,24 @@ include "../config/connection.php";
                     </div>
 
                     <form action="" method="post">
-                    <div class="row">
-                    
-                        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
+                        <div class="row">
+
+                            <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
                                 <div class="card">
                                     <h5 class="card-header"> Search</h5>
                                     <div class="card-body">
 
                                         <div class="form-row">
                                             <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-8 mb-2">
-                                                
+
                                                 <input type="text" class="form-control" id="txtSearch" Name="txtSearch"
-                                                    data-parsley-trigger="change" placeholder="Search by Full name, email or company name">
+                                                    data-parsley-trigger="change"
+                                                    placeholder="Search by Full name, email or company name">
 
                                             </div>
                                             <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
-                                                <button type="submit" class="btn btn-space btn-sm btn-success">Search</button>
+                                                <button type="submit"
+                                                    class="btn btn-space btn-sm btn-success">Search</button>
                                                 <!-- <label for=""> </label>
                                                 <p class="text-center">
                                                     
@@ -65,19 +70,22 @@ include "../config/connection.php";
 
                                     </div>
                                 </div>
+                            </div>
+
                         </div>
-                        
-                    </div>
                     </form>
                     <div class="row">
                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                             <div class="card">
                                 <h5 class="card-header"> Customer List</h5>
-                                
-                                <div class="card-body">
 
+                                <div class="card-body">
+                                    <div class='text-right'>
+                                        <button onclick='generateExcel()' class='btn btn-space btn-sm btn-success'>
+                                            Export Excel</button>
+                                    </div>
                                     <?php
-                                    
+
                                     // Handle the search
                                     $searchTerm = isset($_POST['txtSearch']) ? $_POST['txtSearch'] : '';
 
@@ -86,7 +94,7 @@ include "../config/connection.php";
 
                                     // Check if there are rows in the result
                                     if ($result->num_rows > 0) {
-                                        echo '<table class="table table-hover"> <thead><tr><th scope="col">#</th>
+                                        echo '<table class="table table-hover" id="myTable"> <thead><tr><th scope="col">#</th>
                                     <th scope="col">Fullname</th> <th scope="col">Email</th><th scope="col">Comapny Name</th>
                                     <th scope="col">Register Date</th> <th scope="col">Delete</th>
                                     </tr></thead><tbody>';
@@ -149,25 +157,47 @@ include "../config/connection.php";
 
 
     <script>
-    function deleteRow(rowId) {
-        var confirmDelete = confirm("Are you sure you want to delete this row?");
-        if (confirmDelete) {
-            // Use AJAX to send an asynchronous request to delete the row
-            $.ajax({
-                type: "POST",
-                url: "", // Leave it empty to send the request to the same PHP file
-                data: { id: rowId },
-                success: function(response) {
-                    // Reload the page or update the grid based on your needs
-                    location.reload();
-                },
-                error: function() {
-                    alert("Error deleting row.");
-                }
-            });
+        function deleteRow(rowId) {
+            var confirmDelete = confirm("Are you sure you want to delete this row?");
+            if (confirmDelete) {
+                // Use AJAX to send an asynchronous request to delete the row
+                $.ajax({
+                    type: "POST",
+                    url: "", // Leave it empty to send the request to the same PHP file
+                    data: { id: rowId },
+                    success: function (response) {
+                        // Reload the page or update the grid based on your needs
+                        location.reload();
+                    },
+                    error: function () {
+                        alert("Error deleting row.");
+                    }
+                });
+            }
         }
-    }
-</script>
+    </script>
+    <script>
+        function generateExcel() {
+            const table = document.getElementById('myTable');
+
+            // Hide "View Response" column
+            const viewResponseColumn = table.querySelectorAll('.view-response');
+            viewResponseColumn.forEach(function (column) {
+                column.style.display = 'none';
+            });
+
+            const wb = XLSX.utils.table_to_book(table, {
+                sheet: "SheetJS"
+            });
+
+            // Show "View Response" column again for the design
+            viewResponseColumn.forEach(function (column) {
+                column.style.display = ''; // Reset to default display value
+            });
+
+            XLSX.writeFile(wb, 'respondent_list.xlsx');
+        }
+    </script>
 
 
 </body>
